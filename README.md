3b330eec576f61bf922ec47c40685c7a50615972
Jason Corbett <jsncbt@gmail.com>
2013-11-13 11:31:28 +1000

    Search now gets album names and images

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-stans
-=====
+
+* Save input value into variable.
+* If the value is larger than 3 characters then begin a search of albums.
+* Only show results from 2013.
+* Everytime a key is pressed the value variable is updated.  
--- search.js
+++ search.js
@@ -6,26 +6,47 @@ url = 'http://ws.audioscrobbler.com/2.0/';
 data = {
   api_key: '556717aa07990447a306688c12b23e2b',
   format: 'json',
-  method: 'artist.search',
-  artist: 'action bronson'
+  method: 'album.search',
+  artist: '',
+  limit: 5
 };
 
-handleResponse = function(res) {
-  return console.log(res);
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
 };
 
-doRequest = function(artist) {
-  data.artist = artist;
+var result = _.template('<div class="result"><div class="result__img"><img src="{{imgUrl}}"></div><div class="results__body"><h4>{{name}}</h4></div></div>'),
+
+printAlbums = function(res) {
+  var albums = res.results.albummatches.album;
+  for(i in albums) {
+    var imgUrl = albums[i].image[1]['#text'],
+        name = albums[i].name,
+        html = result({name: name, imgUrl: imgUrl});
+    $('#results').append(html);
+  }
+};
+
+findAlbum = function(album) {
+  data.album = album;
   return $.ajax({
     url: url,
     cache: false,
     dataType: 'json',
     data: data
-  }).done(handleResponse);
+  }).done(printAlbums);
 };
+
+var triggerSearch = _.debounce(function() {
+    var val = $('#search').val();
+    if (val.length > 3) {
+      findAlbum(val);
+    }
+}, 500);
 
 jQuery(function($) {
 	
-	doRequest('Spark Master Tape');
+  $('#search').keyup(triggerSearch);
 
 });
+
